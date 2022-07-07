8ab2dd33ee05a0f468ce25bbeadd03b2b7118a90
Nick A Nichols <nichols1991@gmail.com>
2022-07-06 19:34:05 -0500

    Update Docker Image to Alpine (#3)
    * Output files which will be linted as a group
    
    * Move to alpine image
    
    * Workaround for git v2.35.2 issue
    
    * Install rlwrap so clojure-tools works appropriately
    
    * Send multiple --lint args
    
    * Redact timing information
    
    * Log results of linting
    
    * Fix param passing
    
    * Simplify
    
    * Simplify
    
    * Update documentation

--- lint.sh
+++ lint.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
-cd "${GITHUB_WORKSPACE}" || exit
+cd "${GITHUB_WORKSPACE}" || exit 1
+
+# https://github.com/reviewdog/reviewdog/issues/1158
+git config --global --add safe.directory "$GITHUB_WORKSPACE" || exit 1
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-clj-kondo --lint $(find "${INPUT_PATH}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN}") \
+sources=$(find "${INPUT_PATH}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN}")
+
+echo "::group::Files to lint"
+echo "${sources}"
+echo "::endgroup::"
+
+clj -Sdeps '{:deps {clj-kondo/clj-kondo {:mvn/version "RELEASE"}}}' -M -m clj-kondo.main \
+  --lint $(find "${INPUT_PATH}" -not -path "${INPUT_EXCLUDE}" -type f -name "${INPUT_PATTERN}") \
   --config "${INPUT_CLJ_KONDO_CONFIG}" \
   --config '{:output {:pattern "{{filename}}:{{row}}:{{col}}: {{message}}"}}' \
+  --config '{:summary false}' \
   | reviewdog \
       -efm="%f:%l:%c: %m" \
       -name="clj-kondo" \
@@ -14,4 +25,8 @@ clj-kondo --lint $(find "${INPUT_PATH}" -not -path "${INPUT_EXCLUDE}" -type f -n
       -filter-mode="${INPUT_FILTER_MODE}" \
       -fail-on-error="${INPUT_FAIL_ON_ERROR}" \
       -level="${INPUT_LEVEL}" \
-      ${INPUT_REVIEWDOG_FLAGS}
+      "${INPUT_REVIEWDOG_FLAGS}"
+
+exit_code=$?
+
+exit $exit_code
